d8f1d1307cfc284804500dd3947cd42defb1fcec
Wilson Ndirangu <wndirangu@cytonn.com>
2016-03-22 10:08:11 +0300

    Javascript functions

---
+++ Function.js
@@ -0,0 +1,28 @@
+/**
+ * Cytonn Technologies
+ * @author Ndirangu wilson <wndirangu@cytonn.com>
+ *
+ */
+
+//function to get the fibonacci number at the given term
+function fibonacci(term) {
+
+  return Math.round(Math.pow((Math.sqrt(5) + 1) / 2, term) / Math.sqrt(5));
+
+}
+
+/**
+ * function to compute the energy equivqlent of the mass given
+ * @param  {[float]} mass [the mass of a given substance in grams]
+ * @return {[float]}      [energy equivalent of the mass in joules]
+ */
+function mass_energy_equivalent(mass) {
+  var speed_of_light = 3.0e8;
+  return mass * Math.pow(speed_of_light, 2);
+
+}
+
+
+
+console.log(fibonacci(2));
+console.log(mass_energy_equivalent(0.5));
