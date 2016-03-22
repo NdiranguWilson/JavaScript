699908118e689b540956d131da05085742445fbb
Wilson Ndirangu <wndirangu@cytonn.com>
2016-03-22 11:51:15 +0300

    add prototype file

---
+++ Prototypes.js
@@ -0,0 +1,19 @@
+/**
+ * Cytonn Technologies
+ * @author Ndirangu wilson <wndirangu@Cytonn.com>
+ *
+ */
+
+ var Circle = function(radius) {
+ this.radius = radius;
+
+ }
+
+ //augment Circle's default prototype property thereby augmenting the prototype of each generated instance
+ Circle.prototype.area = function() {
+ return Math.PI*this.radius*this.radius;
+ }
+
+ //creating an  instance of a circle leveranging  common prototype
+ var a = new Circle(3);
+ console.log(a.area().toFixed(2)); 
